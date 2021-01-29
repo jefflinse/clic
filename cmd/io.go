cb540633094544a8ccf94069926fa710ca9628b5
Jeff Linse <jeff@jefflinse.com>
2021-01-28 22:06:04 -0800

    Get rid of io package

---
+++ cmd/io.go
@@ -0,0 +1,105 @@
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/jefflinse/clic/spec"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/afero"
+)
+
+var execRunner func(cmd *exec.Cmd) error
+var fs afero.Fs
+
+// Creates a directory, removing any existing directory if 'force' is true.
+func createDirectory(path string, force bool) error {
+	exists, err := afero.Exists(fs, path)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		isDir, err := afero.IsDir(fs, path)
+		if err != nil {
+			return err
+		}
+
+		if !isDir {
+			return fmt.Errorf("cannot create directory '%s': directory exists", path)
+		}
+
+		if force {
+			if rmErr := fs.RemoveAll(path); rmErr != nil {
+				return err
+			}
+		}
+
+	}
+
+	return fs.MkdirAll(path, 0755)
+}
+
+func newAppFromDirectory(path string) (spec.App, error) {
+	var specFiles []string
+	for extension := range specFileUnmarshalers {
+		files, _ := filepath.Glob(filepath.Join(path, "*."+extension))
+		specFiles = append(specFiles, files...)
+	}
+
+	var specs []spec.App
+	for _, file := range specFiles {
+		appSpec, err := newAppFromFile(file)
+		if err != nil {
+			return spec.App{}, err
+		}
+		specs = append(specs, appSpec)
+	}
+
+	log.Debug().Str("type", "directory").Str("path", path).Msg("read app spec")
+	return spec.MergeAppSpecs(specs...)
+}
+
+func newAppFromFile(path string) (spec.App, error) {
+	content, err := afero.ReadFile(fs, path)
+	if err != nil {
+		return spec.App{}, err
+	}
+
+	extension := strings.Split(filepath.Base(path), ".")[1]
+	if unmarshaler, ok := specFileUnmarshalers[extension]; ok {
+		var app spec.App
+		if err := unmarshaler(content, &app); err != nil {
+			return app, err
+		}
+
+		log.Debug().Str("type", "file").Str("path", path).Msg("read app spec")
+		return app, nil
+	}
+
+	return spec.App{}, fmt.Errorf("unsupported file extension '%s'", extension)
+}
+
+func newAppFromPath(path string) (spec.App, error) {
+	isDir, err := afero.IsDir(fs, path)
+	if err != nil {
+		return spec.App{}, err
+	}
+
+	if isDir {
+		return newAppFromDirectory(path)
+	}
+
+	exists, err := afero.Exists(fs, path)
+	if err != nil {
+		return spec.App{}, err
+	}
+
+	if exists {
+		return newAppFromFile(path)
+	}
+
+	return spec.App{}, fmt.Errorf("'%s' does not exist", path)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -8,11 +8,9 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
-	"strings"
 
 	"github.com/goccy/go-yaml"
 	gobuilder "github.com/jefflinse/clic/builder/go"
-	"github.com/jefflinse/clic/io"
 	"github.com/jefflinse/clic/spec"
 	gowriter "github.com/jefflinse/clic/writer/go"
 	"github.com/rs/zerolog"
@@ -21,8 +19,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var execRunner func(cmd *exec.Cmd) error
-var fs afero.Fs
 var rootCmd *cobra.Command
 var specFileUnmarshalers map[string]func(data []byte, v interface{}) error = map[string]func(data []byte, v interface{}) error{
 	"json": json.Unmarshal,
@@ -108,12 +104,13 @@ func prerun(cmd *cobra.Command, args []string) {
 	})
 
 	dryRun, _ := cmd.Flags().GetBool("dry-run")
-	fs = io.Init(dryRun)
 	if dryRun {
+		fs = afero.NewCopyOnWriteFs(afero.NewOsFs(), afero.NewMemMapFs())
 		execRunner = func(cmd *exec.Cmd) error {
 			return nil
 		}
 	} else {
+		fs = afero.NewOsFs()
 		execRunner = func(cmd *exec.Cmd) error {
 			return cmd.Run()
 		}
@@ -185,7 +182,7 @@ func generate(cmd *cobra.Command, args []string) error {
 		outputDir = path.Join(cwd, outputDir)
 	}
 
-	if err := io.CreateDirectory(outputDir, false); err != nil {
+	if err := createDirectory(outputDir, false); err != nil {
 		return err
 	}
 
@@ -213,61 +210,4 @@ func loadAppSpec(path string) (spec.App, error) {
 
 	log.Info().Msg("validating app spec")
 	return app.Validate()
-}
-
-// NewAppFromPath create a new app spec from the specified file or directory.
-func newAppFromPath(path string) (spec.App, error) {
-	if t, err := io.PathType(path); err != nil {
-		return spec.App{}, err
-	} else if t == io.Nonexistent {
-		return spec.App{}, fmt.Errorf("path '%s' does not exist", path)
-	} else if t == io.File {
-		log.Debug().Str("type", "file").Str("path", path).Msg("app spec")
-		return newAppFromFile(path)
-	} else if t == io.Directory {
-		log.Debug().Str("type", "directory").Str("path", path).Msg("app spec")
-		return newAppFromDirectory(path)
-	} else {
-		log.Debug().Msgf("----> %d", t)
-	}
-
-	return spec.App{}, fmt.Errorf("unexepcted error regarding path '%s'", path)
-}
-
-func newAppFromDirectory(path string) (spec.App, error) {
-	var specFiles []string
-	for extension := range specFileUnmarshalers {
-		files, _ := filepath.Glob(filepath.Join(path, "*."+extension))
-		specFiles = append(specFiles, files...)
-	}
-
-	var specs []spec.App
-	for _, file := range specFiles {
-		appSpec, err := newAppFromFile(file)
-		if err != nil {
-			return spec.App{}, err
-		}
-		specs = append(specs, appSpec)
-	}
-
-	return spec.MergeAppSpecs(specs...)
-}
-
-func newAppFromFile(path string) (spec.App, error) {
-	content, err := afero.ReadFile(fs, path)
-	if err != nil {
-		return spec.App{}, err
-	}
-
-	extension := strings.Split(filepath.Base(path), ".")[1]
-	if unmarshaler, ok := specFileUnmarshalers[extension]; ok {
-		var app spec.App
-		if err := unmarshaler(content, &app); err != nil {
-			return app, err
-		}
-
-		return app, nil
-	}
-
-	return spec.App{}, fmt.Errorf("unsupported file extension '%s'", extension)
 }
